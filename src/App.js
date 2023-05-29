cc392d18402551a1883831daaa3d62f364d3a036
lavantien <lavantien96@gmail.com>
2023-05-29 08:28:11 +0700

    use type button for buttons

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 export default function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    function handleIncClick() {
-        setCount(count + 1);
-    }
+  function handleIncClick() {
+    setCount(count + 1);
+  }
 
-    function handleDecClick() {
-        setCount(count - 1);
-    }
+  function handleDecClick() {
+    setCount(count - 1);
+  }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>{count}</h1>
-                <IncButton onClick={handleIncClick}>Increment</IncButton>
-                <DecButton onClick={handleDecClick}>Decrement</DecButton>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{count}</h1>
+        <IncButton onClick={handleIncClick}>Increment</IncButton>
+        <DecButton onClick={handleDecClick}>Decrement</DecButton>
+      </header>
+    </div>
+  );
 }
 
 function IncButton({ onClick }) {
-    return (
-        <button onClick={onClick}>
-        +
-        </button>
-    )
+  return (
+    <button type="button" onClick={onClick}>
+      +
+    </button>
+  );
 }
 
 function DecButton({ onClick }) {
-    return (
-        <button onClick={onClick}>
-        -
-        </button>
-    )
+  return (
+    <button type="button" onClick={onClick}>
+      -
+    </button>
+  );
 }
